ee0c4155af57b02e81530ae9643dd5b9271dd0cf
Leonard <l.vasseur@etudiant.ecolemelies.fr>
2021-10-10 10:34:23 +0200

    jouer et quitter marchent

--- Assets/menu.cs
+++ Assets/menu.cs
@@ -3,11 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class menu : MonoBehaviour
+public class Menu : MonoBehaviour
 {
-    public void PlayGame ()
+    [SerializeField] GameObject loadingScreenPrefab;
+    public void LoadSceneAsync()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        StartCoroutine(LoadingScreenCoroutine());
+    }
+
+    private IEnumerator LoadingScreenCoroutine()
+    {
+        var prefab = Instantiate(loadingScreenPrefab);
+        DontDestroyOnLoad(prefab);
+        var sceneLoading = SceneManager.LoadSceneAsync("2D_top-down"); // permet de charger la scène en décaler
+        sceneLoading.allowSceneActivation = false; // bloque l'affichage de la scène suivante.
+        while (sceneLoading.isDone == false)
+        {
+            if (sceneLoading.progress >= 0.9f) //chargement dans unity est à 90% le rest c'est l'affichage sur le screen
+            {
+                sceneLoading.allowSceneActivation = true; //réautorise l'affichage de la scène game.
+                prefab.SetActive(false);
+            }
+            yield return new WaitForSeconds(1);
+        }
+        //yield return new WaitForSeconds(2);
+        //SceneManager.LoadScene("Game");
+        //yield return null;
+    }
+
+    /*public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu"); // pour les boutons back
+    }
+
+    public void ToOptions()
+    {
+        SceneManager.LoadScene("Options");
+    }*/
+
+    public void ToQuit()
+    {
+        Application.Quit();
     }
 
 }
